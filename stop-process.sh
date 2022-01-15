97d4e46553e68e786bb48872111ce6d9093c628f
Absar Hussain <absar.hussain@royalcyber.com>
2022-01-15 19:21:53 +0500

    stop

--- stop-process.sh
+++ stop-process.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-ps -ef | grep cfs-service-registry-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{print $2}' | xargs kill
+ps aux
+#ps -ef | grep cfs-service-registry-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{print $2}' | xargs kill
